4a7d80afc98583abb8fb135f27d08d77545e6852
Alloy-S <107180700+Alloy-S@users.noreply.github.com>
2025-08-28 14:50:47 +0700

    config: increase db pool

--- app/config.py
+++ app/config.py
@@ -9,3 +9,8 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'kunci-rahasia-default-untuk-dev')
     SQLALCHEMY_ECHO = os.getenv('SQLALCHEMY_ECHO', 'False').lower() in ('true', '1', 't')
+
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        'pool_size': 10,
+        'max_overflow': 20
+    }
